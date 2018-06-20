2f78d217ad3a342724b73e6e1bbc2f747dc779ed
Ning Wang <nyngwang@gmail.com>
2018-06-21 00:54:10 +0800

    更新lv4_courses pattern predict測試程式碼。

--- lv4_courses_pattern_predict.py
+++ lv4_courses_pattern_predict.py
@@ -15,15 +15,6 @@ test_student_ids = ['05090432', '06090130',
                     '06130274', '05130861',
                     '06540611', '05541341']
 
-cluster_patterns = compute_lv4_frequent_patterns(7000)
-print('Total number of patterns: {}'.format(len(cluster_patterns)))
-
-# Choose only pattern with length >= 7
-cluster_patterns_ge7 = lv4_patterns_filter(cluster_patterns, min_len=8)
-
-# Compute the difference between student record & patterns for each.
-diff_sets = compute_lv4_diff_sets(test_student_ids, cluster_patterns_ge7)
-
 # recommend courses for each student
 for student_id in test_student_ids:
     recommendations = lv4_courses_recommendation_service(student_id)
